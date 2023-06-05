22aa818b5bd8a1607e9d7b556c6b6ff356b99f41
HypeFish <99514952+HypeFish@users.noreply.github.com>
2023-06-05 00:40:47 -0400

    Wind owwww

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 
 export default function Home() {
-  
   //make a page to detail the download of an app named Hyperate4Health
   return (
     <main  className="flex min-h-screen flex-col items-center justify-between">
