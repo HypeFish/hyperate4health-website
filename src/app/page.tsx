efa0fe954f4242abb5f079090f10adfb6e650988
HypeFish <99514952+HypeFish@users.noreply.github.com>
2023-06-29 14:48:50 -0400

    intel chip who?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,11 +14,19 @@ export default function Home() {
           Hyperate4Health is a desktop app that helps you track your heart rate and blood pressure. Designed as a tool used for research, Hyperate4Health is a free and open source app that can be used by anyone. Hyperate4Health is available for MacOS and Windows. Hyperate4Health is compatible with any device which can connect to the Hyperate website.
         </p>
         <p className = "relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-          In order to use Hyperate4Health, you must have Java/JDK 20 installed on your computer. If you do not have Java installed, you can download it here: <a href="https://www.oracle.com/java/technologies/downloads/">Java Download</a>
+          In order to use Hyperate4Health, you must have Java/JDK 20 installed on your computer. If you do not have Java installed, you can download it here: 
+          
+        </p>
+        <p>
+          <a href="https://www.oracle.com/java/technologies/downloads/">Java Download</a>
         </p>
 
         <p>
-          <a href='https://www.mediafire.com/file/fjg0joj0auk0gm7/Hyperate4Health-1.0.zip/file' download> Download ZIP for MacOS</a>
+          <a href='https://www.mediafire.com/file/fjg0joj0auk0gm7/Hyperate4Health-1.0.zip/file' download> Download ZIP for MacOS (Apple Silicon)</a>
+        </p>
+
+        <p>
+          <a href ='https://www.mediafire.com/file/xrszq2hflm19svl/Hyperate4Health-1.0.0.dmg.zip/file'  download> Download ZIP for MacOS (Intel)</a>
         </p>
 
         <p>
